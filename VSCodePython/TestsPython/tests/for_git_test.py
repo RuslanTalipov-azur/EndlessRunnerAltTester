4ab7c2d83b6c0d29fde89a496461c866c72a82df
TalipovR <talipov.r@azurgames.com>
2025-02-11 13:26:02 +0500

    add new check number

--- VSCodePython/TestsPython/tests/for_git_test.py
+++ VSCodePython/TestsPython/tests/for_git_test.py
@@ -1,3 +1,10 @@
 from pages.main_menu_page import MainMenuPage
 from pages.main_menu_popup import MainMenuPopup
 import pytest
+
+i = 112
+
+if i // 2 != 0:
+    print(i, 'нечётное')
+else:
+    print(i, 'чётное')
